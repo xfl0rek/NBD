109ebc1ac3a70b0492f9cce63a76882357e02963
Wojciech Florczak <florczak.wojciechv@gmail.com>
2024-10-07 20:27:49 +0200

    Added Rent class; testing in main.

--- src/main/java/pl/nbd/Main.java
+++ src/main/java/pl/nbd/Main.java
@@ -1,7 +1,21 @@
 package pl.nbd;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Address address = new Address("Real", "Madryt", "7");
+        Premium premium = new Premium();
+        Default defaults = new Default();
+        Client client = new Client("Cristiano", "Ronaldo", "123456789", address, premium);
+        Room room = new Room(1000, 7, 2);
+        Rent rent = new Rent(UUID.randomUUID(), client, room, LocalDateTime.now());
+        LocalDateTime endTime = LocalDateTime.now().plus(Duration.ofHours(168));
+        rent.endRent(endTime);
+
+        System.out.println("Liczba dni wynajmu: " + rent.getRentDays());
+        System.out.println("Ostateczny koszt wynajmu: " + rent.getRentCost());
     }
 }
