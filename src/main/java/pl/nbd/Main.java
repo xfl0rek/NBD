2a57f6755e324d5b9db78433babb1ab45c29269d
Wojciech Florczak <florczak.wojciechv@gmail.com>
2024-10-08 17:10:11 +0200

    Save changes to ORM branch.

--- src/main/java/pl/nbd/Main.java
+++ src/main/java/pl/nbd/Main.java
@@ -7,9 +7,9 @@ import java.util.UUID;
 public class Main {
     public static void main(String[] args) {
         Address address = new Address("Real", "Madryt", "7");
-        PremiumType premium = new PremiumType();
-        DefaultType defaults = new DefaultType();
-        Client client = new Client("Cristiano", "Ronaldo", "123456789", address, premium);
+        PremiumType premiumType = new PremiumType();
+        DefaultType defaultType = new DefaultType();
+        Client client = new Client("Cristiano", "Ronaldo", "123456789", address, premiumType);
         Room room = new Room(1000, 7, 2);
         Rent rent = new Rent(UUID.randomUUID(), client, room, LocalDateTime.now());
         LocalDateTime endTime = LocalDateTime.now().plus(Duration.ofHours(168));
