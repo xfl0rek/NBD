d2d82484e705809d5e39d42f3aa78bc8ef908550
Wojtyla23 <247734@edu.p.lodz.pl>
2024-11-23 00:13:15 +0100

    Redis create and read implementation and unit tests

--- src/main/java/pl/nbd/repository/RedisRoomRepository.java
+++ src/main/java/pl/nbd/repository/RedisRoomRepository.java
@@ -4,6 +4,10 @@ import jakarta.json.bind.Jsonb;
 import jakarta.json.bind.JsonbBuilder;
 import pl.nbd.model.Room;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 public class RedisRoomRepository extends AbstractRedisRepository {
 
     private String hashPrefix = "room:";
@@ -14,16 +18,41 @@ public class RedisRoomRepository extends AbstractRedisRepository {
     }
 
     public void save(Room room) {
-        String key = hashPrefix + Long.toString(room.getRoomNumber());
-        String json = jsonb.toJson(room);
-        System.out.println(json);
-        pool.set(key, json);
+        try {
+            String key = hashPrefix + Long.toString(room.getRoomNumber());
+            String json = jsonb.toJson(room);
+            System.out.println(json);
+            pool.set(key, json);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public Room findByRoomNumber(long roomNumber) {
-        String key = hashPrefix + Long.toString(roomNumber);
-        String json =  pool.get(key);
-        return jsonb.fromJson(json, Room.class);
+        try {
+            String key = hashPrefix + Long.toString(roomNumber);
+            String json = pool.get(key);
+            return jsonb.fromJson(json, Room.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public List<Room> findAll() {
+        try {
+            List<Room> rooms = new ArrayList<>();
+            Set<String> keys = pool.keys(hashPrefix + "*");
+            for (String key : keys) {
+                String json = pool.get(key);
+                Room room = jsonb.fromJson(json, Room.class);
+                rooms.add(room);
+            }
+            return rooms;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
 
--- src/test/java/pl/nbd/managers/RedisRoomTest.java
+++ src/test/java/pl/nbd/managers/RedisRoomTest.java
@@ -1,23 +1,42 @@
 package pl.nbd.managers;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import pl.nbd.model.Room;
 import pl.nbd.model.RoomChildren;
+import pl.nbd.model.RoomRegular;
 import pl.nbd.repository.RedisRoomRepository;
+
+import java.util.List;
 
 public class RedisRoomTest {
 
     @Test
-    public void test() {
+    public void createAndReadRoomFromRedis() {
         Room room = new RoomChildren(1, 100, 4, 2);
-        RedisRoomRepository rep = new RedisRoomRepository();
-        System.out.println(room.getClass());
-        rep.save(room);
-        RoomChildren room1 = (RoomChildren) rep.findByRoomNumber(1);
+        RedisRoomRepository redisRoomRepository = new RedisRoomRepository();
+        redisRoomRepository.save(room);
 
-        System.out.println(room1.getRoomNumber());
-        System.out.println(room1.getRoomCapacity());
-        System.out.println(room1.getBasePrice());
+        Room roomFromRedis = redisRoomRepository.findByRoomNumber(1);
+        Assertions.assertEquals(room, roomFromRedis);
+    }
 
+    @Test
+    public void readAllRoomsFromRedis() {
+        Room room1 = new RoomChildren(1, 100, 4, 2);
+        Room room2 = new RoomRegular(2, 50, 2, true);
+        Room room3 = new RoomChildren(3, 150, 5, 1);
+
+        RedisRoomRepository redisRoomRepository = new RedisRoomRepository();
+        redisRoomRepository.save(room1);
+        redisRoomRepository.save(room2);
+        redisRoomRepository.save(room3);
+
+        List<Room> rooms = redisRoomRepository.findAll();
+
+        Assertions.assertEquals(3, rooms.size());
+        Assertions.assertEquals(room1, rooms.get(0));
+        Assertions.assertEquals(room2, rooms.get(1));
+        Assertions.assertEquals(room3, rooms.get(2));
     }
 }
