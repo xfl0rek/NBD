a4d2f79844fa283be84d93c1bc9f6467db091b00
Wojciech Florczak <florczak.wojciechv@gmail.com>
2025-01-23 19:48:43 +0100

    golem

--- producent/src/main/java/pl/nbd/App.java
+++ producent/src/main/java/pl/nbd/App.java
@@ -1,24 +1,24 @@
-package pl.nbd;
-
-import pl.nbd.model.*;
-import pl.nbd.Consumer;
-import pl.nbd.KafkaProducent;
-import pl.nbd.model.*;
-
-import java.time.LocalDateTime;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        Address address = new Address("Laczna", "Lipinki", "43");
-        Client client = new DefaultClient(1, "Syn", "Hymel", address);
-        Room room = new RoomRegular(1, 100, 2, true);
-        LocalDateTime startDate = LocalDateTime.now();
-        Rent rent = new Rent(1, client, room, startDate);
-        KafkaProducent kafkaProducent = new KafkaProducent();
-        while (true) {
-            kafkaProducent.sendRent(rent);
-            Thread.sleep(10000);
-        }
-
-    }
-}
+//package pl.nbd;
+//
+//import pl.nbd.model.*;
+//import pl.nbd.Consumer;
+//import pl.nbd.KafkaProducent;
+//import pl.nbd.model.*;
+//
+//import java.time.LocalDateTime;
+//
+//public class App {
+//    public static void main(String[] args) throws Exception {
+//        Address address = new Address("Laczna", "Lipinki", "43");
+//        Client client = new DefaultClient(1, "Syn", "Hymel", address);
+//        Room room = new RoomRegular(1, 100, 2, true);
+//        LocalDateTime startDate = LocalDateTime.now();
+//        Rent rent = new Rent(1, client, room, startDate);
+//        KafkaProducent kafkaProducent = new KafkaProducent();
+//        while (true) {
+//            kafkaProducent.sendRent(rent);
+//            Thread.sleep(10000);
+//        }
+//
+//    }
+//}
--- producent/src/main/java/pl/nbd/Main.java
+++ producent/src/main/java/pl/nbd/Main.java
@@ -1,14 +1,22 @@
 package pl.nbd;
 
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import pl.nbd.model.*;
+
+import java.time.LocalDateTime;
+import java.util.concurrent.ExecutionException;
 
 public class Main {
-    public static void main(String[] args) {
-        Consumer consumer = new Consumer(3);
-        consumer.initConsumers();
-
-        for (KafkaConsumer<Long, String> kafkaConsumer : consumer.getKafkaConsumers()) {
-            new Thread(() -> consumer.consume(kafkaConsumer)).start();
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        Address address = new Address("Laczna", "Lipinki", "43");
+        Client client = new DefaultClient(1, "Syn", "Hymel", address);
+        Room room = new RoomRegular(1, 100, 2, true);
+        LocalDateTime startDate = LocalDateTime.now();
+        Rent rent = new Rent(1, client, room, startDate);
+        KafkaProducent kafkaProducent = new KafkaProducent();
+        while (true) {
+            kafkaProducent.sendRent(rent);
+            Thread.sleep(10000);
         }
     }
 }
